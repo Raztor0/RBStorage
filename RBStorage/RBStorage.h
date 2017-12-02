77eadfcd83246d207264ce08017436508243634e
Razvan Bangu <razvan.bangu@gmail.com>
2017-12-02 01:16:25 -0500

    Initial commit

---
+++ RBStorage/RBStorage.h
@@ -0,0 +1,19 @@
+//
+//  RBStorage.h
+//  RBStorage
+//
+//  Created by Razvan Bangu on 2017-12-02.
+//  Copyright © 2017 Razio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RBStorage.
+FOUNDATION_EXPORT double RBStorageVersionNumber;
+
+//! Project version string for RBStorage.
+FOUNDATION_EXPORT const unsigned char RBStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RBStorage/PublicHeader.h>
+
+
